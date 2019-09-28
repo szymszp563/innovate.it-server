209d29c9c795296d423a10c4edfc25f3afef454b
Kyeiv <krzy.poloczek@gmail.com>
2019-09-29 00:56:20 +0200

    get investitions added by user

--- src/main/java/com/hackathon/server/rest/controller/InvestitionRestController.java
+++ src/main/java/com/hackathon/server/rest/controller/InvestitionRestController.java
@@ -12,10 +12,7 @@ import com.hackathon.server.service.InvestitionService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -61,6 +58,18 @@ public class InvestitionRestController {
             return new ResponseEntity<>(investitionResponse, HttpStatus.OK);
         }
 
+    }
+
+    @GetMapping("/investitions/{username}")
+    public ResponseEntity getInvestitionsGradedByUser(
+            @PathVariable("username") String username
+    ) throws UserNotFoundException, InvestitionNotFoundException {
+        List<InvestitionDto> investitionDtos =
+                investitionService.findInvestitionWithGradeOfUser(username)
+                .stream().map(investitionMapper::investiotionToInvestitionDto).collect(Collectors.toList());
+
+        InvestitionsResponse investitionResponse = getInvestitionsResponse(investitionDtos);
+        return new ResponseEntity<>(investitionResponse, HttpStatus.OK);
     }
 
     private InvestitionsResponse getInvestitionsResponse(List<InvestitionDto> investitionDtos) {
