67581a969448063ac3f683f545a8b6c64e2e6078
Kyeiv <krzy.poloczek@gmail.com>
2019-09-28 15:04:10 +0200

    removed depracated

--- src/main/java/com/hackathon/server/config/ServerConfig.java
+++ src/main/java/com/hackathon/server/config/ServerConfig.java
@@ -20,31 +20,31 @@ public class ServerConfig {
 
     private Logger logger = Logger.getLogger(getClass().getName());
 
-    @Bean
-    public DataSource secutiyDataSource() {
-
-        ComboPooledDataSource securityDataSource = new ComboPooledDataSource();
-
-        try {
-            securityDataSource.setDriverClass(environment.getProperty("spring.datasource.driver-class-name"));
-        } catch (PropertyVetoException e) {
-            throw new RuntimeException(e);
-        }
-
-        logger.info(">>> jdbc.url= " + environment.getProperty("spring.datasource.url"));
-        logger.info(">>> jdbc.user= " + environment.getProperty("spring.datasource.username"));
-
-        securityDataSource.setJdbcUrl(environment.getProperty("spring.datasource.url"));
-        securityDataSource.setUser(environment.getProperty("spring.datasource.username"));
-        securityDataSource.setPassword(environment.getProperty("spring.datasource.password"));
-
-        securityDataSource.setInitialPoolSize(getIntegerProperty("connection.pool.initialPoolSize"));
-        securityDataSource.setMinPoolSize(getIntegerProperty("connection.pool.minPoolSize"));
-        securityDataSource.setMaxPoolSize(getIntegerProperty("connection.pool.maxPoolSize"));
-        securityDataSource.setMaxIdleTime(getIntegerProperty("connection.pool.maxIdleTime"));
-
-        return securityDataSource;
-    }
+//    @Bean
+//    public DataSource secutiyDataSource() {
+//
+//        ComboPooledDataSource securityDataSource = new ComboPooledDataSource();
+//
+//        try {
+//            securityDataSource.setDriverClass(environment.getProperty("spring.datasource.driver-class-name"));
+//        } catch (PropertyVetoException e) {
+//            throw new RuntimeException(e);
+//        }
+//
+//        logger.info(">>> jdbc.url= " + environment.getProperty("spring.datasource.url"));
+//        logger.info(">>> jdbc.user= " + environment.getProperty("spring.datasource.username"));
+//
+//        securityDataSource.setJdbcUrl(environment.getProperty("spring.datasource.url"));
+//        securityDataSource.setUser(environment.getProperty("spring.datasource.username"));
+//        securityDataSource.setPassword(environment.getProperty("spring.datasource.password"));
+//
+//        securityDataSource.setInitialPoolSize(getIntegerProperty("connection.pool.initialPoolSize"));
+//        securityDataSource.setMinPoolSize(getIntegerProperty("connection.pool.minPoolSize"));
+//        securityDataSource.setMaxPoolSize(getIntegerProperty("connection.pool.maxPoolSize"));
+//        securityDataSource.setMaxIdleTime(getIntegerProperty("connection.pool.maxIdleTime"));
+//
+//        return securityDataSource;
+//    }
 
     private  Integer getIntegerProperty(String propertyName) {
         String propertyValue = environment.getProperty(propertyName);
