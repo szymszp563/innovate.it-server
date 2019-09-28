f40911ceb81487d288cb7641e2266f8764a77dec
Kyeiv <krzy.poloczek@gmail.com>
2019-09-28 17:27:13 +0200

    Merge branch 'master' of https://github.com/szymszp563/innovate.it-server
    # Conflicts:
    #	src/main/java/com/hackathon/server/dto/InvestitionDto.java

--- src/main/java/com/hackathon/server/dto/ImageDto.java
+++ src/main/java/com/hackathon/server/dto/ImageDto.java
@@ -1,5 +1,14 @@
 package com.hackathon.server.dto;
 
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@Getter
+@Setter
+
 public class ImageDto {
 
     private String name;
